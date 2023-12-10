29bb42f250fff8f179dd54f5cfc7aff22dca8d8c
Fabrício de Lima <fablima321@gmail.com>
2023-12-10 02:18:20 -0300

    Adicionada funcionalidade de multiplicação

--- calculator.js
+++ calculator.js
@@ -1,5 +1,37 @@
-function calculate() {
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+
+
+function calculate(operation) {
     var num1 = parseFloat(document.getElementById('num1').value);
     var num2 = parseFloat(document.getElementById('num2').value);
-    document.getElementById('resultado').innerText = 'Resultado: ' + (num1 + num2);
+    var result;
+
+    switch(operation) {
+        case 'add':
+            result = add(num1, num2);
+            break;
+        case 'subtract':
+            result = subtract(num1, num2);
+            break;
+        case 'multiply':
+            result = multiply(num1, num2);
+            break;    
+        
+            
+        default:
+            result = 'Operação inválida';
+    }
+
+    document.getElementById('resultado').innerText = 'Resultado: ' + result;
 }
